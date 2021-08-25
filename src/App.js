ebc59e9be5575d5217d6c95c7a79a49b0e1bf0c8
yahya <crazy20046@gmail.com>
2021-08-25 15:36:27 +0200

    AddBook file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Books from './components/books';
 import Categories from './components/Categories';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 const AddBook = () => {
-  return <div>lol </div>;
+  return (
+    <div>
+      <input type="text" placeholder="new book" />
+      <select id="cars" name="cars">
+        <option>Action</option>
+        <option>Comedy</option>
+        <option>Drama</option>
+        <option>Horror</option>
+        <option>Romance</option>
+      </select>
+      <button type="button">Add Book</button>
+    </div>
+  );
 };
 
 export default AddBook;
